8a4bd7b88637a9ebd4d16d4699e6970dbae41678
Juan Sánchez García <sanjuanperales89@gmail.com>
2016-04-13 13:50:01 +0200

    subida de proyecto

---
+++ src/definitiva/Definitiva.java
@@ -0,0 +1,26 @@
+package definitiva;
+
+public final class Definitiva 
+{
+	
+	private String atr1;
+	private String atr2;
+	
+	public String getAtr1() 
+	{
+		return atr1;
+	}
+	public void setAtr1(String atr1) 
+	{
+		this.atr1 = atr1;
+	}
+	public String getAtr2() 
+	{
+		return atr2;
+	}
+	public void setAtr2(String atr2) 
+	{
+		this.atr2 = atr2;
+	}
+	
+}
